979a0ca2482039ff6b3a06d536abdf4b2e6c8955
Ubuntu <ubuntu@ip-172-31-25-41.ap-northeast-2.compute.internal>
2024-10-16 06:42:33 +0000

    message chage

--- src/main/java/il/ac/kinneret/mjmay/Gradling.java
+++ src/main/java/il/ac/kinneret/mjmay/Gradling.java
@@ -1,6 +1,6 @@
 package il.ac.kinneret.mjmay;public class Gradling {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("!!!JENKINS BUILD SUCCESS!!!");
     }
 
     public static int add(int a, int b) {
